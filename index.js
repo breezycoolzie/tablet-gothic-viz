e441acfa801bc6f380153a9d0b1864c68c292e6f
breezycoolzie <91929894+breezycoolzie@users.noreply.github.com>
2025-06-05 07:18:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const dscc = require('@google/dscc');
 
+function drawViz(data) {
+  const container = document.getElementById("viz");
+  const value = data.tables.DEFAULT[0]?.dimensions[0]?.value || "No data";
+  
+  container.innerHTML = `
+    <div class="tablet-gothic-text">
+      ${value}
+    </div>
+  `;
+}
+
+dscc.subscribeToData(drawViz, { transform: dscc.tableTransform });
